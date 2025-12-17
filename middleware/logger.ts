c8ad1f3cbb93a023b655cbb12f80bb9aeee794fb
Trevor-akshay <trevorakshay@gmail.com>
2025-12-17 16:29:27 -0500

    Fixed logger middleware with next() call

--- middleware/logger.ts
+++ middleware/logger.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
-const logger = (req: Request, _res: Response, _next: NextFunction) => {
+const logger = (req: Request, _res: Response, next: NextFunction) => {
   console.log(`Request Method: ${req.method}, API: ${req.url}`);
+  next();
 };
 
 export default logger;
