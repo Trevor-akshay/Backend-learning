0b0d19de87619a241ff15026de14b7caf46928c8
Trevor-akshay <trevorakshay@gmail.com>
2025-12-17 16:05:26 -0500

    get all users without db

--- routes/Users.ts
+++ routes/Users.ts
@@ -2,7 +2,13 @@ import { Request, Response, NextFunction, Router } from "express";
 
 const userRouter = Router();
 
+const users : any[] = []
+
 userRouter.get("/", (req: Request, res: Response, next: NextFunction) => {
-	
+	res.status(200).json({
+		data : users
+	})
 });
+
+
 export default userRouter;
