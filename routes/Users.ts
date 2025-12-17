8df1c64b2cd7c0407f426ec950de2d194c38068a
Trevor-akshay <trevorakshay@gmail.com>
2025-12-17 16:07:27 -0500

    get user with id without db

--- routes/Users.ts
+++ routes/Users.ts
@@ -10,5 +10,20 @@ userRouter.get("/", (req: Request, res: Response, next: NextFunction) => {
 	})
 });
 
+userRouter.get("/:id", (req: Request, res: Response, next: NextFunction) => {
+  const id = req.params.id;
+
+  const user = users.find((u) => u.id === id);
+
+  if(!user) {
+	return res.status(404).json({
+		message: "User not found"
+	})
+  };
+
+  res.status(200).json({
+    data: user,
+  });
+});
 
 export default userRouter;
