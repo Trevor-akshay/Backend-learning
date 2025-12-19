965dbd0333dd629d9f4f3edcd86ca2f6db8ce242
Trevor-akshay <trevorakshay@gmail.com>
2025-12-19 18:33:41 -0500

    Added class to controller

---
+++ controllers/Users.controller.ts
@@ -0,0 +1,81 @@
+import { Request, Response, NextFunction, Router } from "express";
+import { validationError } from "../utils/utils.ts";
+import UserService from "../service/Users.service.ts";
+
+class UserController {
+  userService: UserService;
+  constructor(userService: UserService) {
+    this.userService = userService;
+  }
+
+  getUsers = async (req: Request, res: Response) => {
+    const { sort, filter } = req.query as {
+      sort: string;
+      filter: string;
+    };
+
+    const users = await this.userService.getUsers(sort, filter);
+
+    res.status(200).json({
+      users,
+    });
+  };
+
+  getuser = async (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    if (Number.isNaN(id))
+      return validationError(400, "User id must be a number", req, res);
+
+    const user = await this.userService.getUser(id);
+
+    res.status(200).json({
+      data: user,
+    });
+  };
+
+  addUser = async (req: Request, res: Response) => {
+    const { name, email, password, orders } = req.body;
+
+    if (!name) return validationError(400, "User name is required", req, res);
+    if (!email) return validationError(400, "User email is required", req, res);
+    if (!password)
+      return validationError(400, "User password is required", req, res);
+
+    const user = await this.userService.postUser({
+      name,
+      email,
+      password,
+      orders,
+    });
+    return res.status(201).json({
+      message: "User added",
+      data: user,
+    });
+  };
+
+  deleteUser = (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    if (Number.isNaN(id))
+      return validationError(400, "User id must be a number", req, res);
+
+    this.userService.deleteUser(id);
+
+    res.status(202).json({
+      message: "User Deleted",
+    });
+  };
+
+  updateUser = (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    if (Number.isNaN(id))
+      return validationError(400, "User id must be a number", req, res);
+
+    this.userService.updateUser(id, { ...req.body });
+
+    res.status(202).json({
+      message: "User Updated",
+    });
+  };
+}
+
+export default UserController;
