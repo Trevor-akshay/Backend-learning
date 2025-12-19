3bc1e19a15cadcea5df42d9037c53d6df4093c63
Trevor-akshay <trevorakshay@gmail.com>
2025-12-19 18:32:56 -0500

    Added gb connection

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import errorHandler from "./middleware/errorHandler.ts";
 import logger from "./middleware/logger.ts";
-import userRouter from "./routes/Users.ts";
+import userRouter from "./routes/Users.routes.ts";
+import { prisma } from "./data/prisma.ts";
 
 const app = express();
 app.use(express.json());
@@ -16,7 +17,11 @@ app.use("*all", (req, res) => {
     message: "Endpoint unavailable",
   });
 });
+
 app.use(errorHandler);
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`server listening on ${PORT}`);
+  await prisma.$connect().then(() => {
+    console.log(`Database connected successfully.`);
+  });
 });
