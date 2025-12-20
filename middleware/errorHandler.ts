62ef038f9a1fd8e2078253f3721b97ec62a0b426
Trevor-akshay <trevorakshay@gmail.com>
2025-12-20 13:14:52 -0500

    custom prismaerror

--- middleware/errorHandler.ts
+++ middleware/errorHandler.ts
@@ -1,10 +1,17 @@
 import { Request, Response, NextFunction } from "express";
+import { PrismaError } from "../utils/utils.ts";
 const errorHandler = (
   err: unknown,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  if (err instanceof PrismaError) {
+    return res.status(err.status).json({
+      message: err.message,
+    });
+  }
+
   if (err instanceof Error) {
     return res.status(400).json({
       message: err.message,
